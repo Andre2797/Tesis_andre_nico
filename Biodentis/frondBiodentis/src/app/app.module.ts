670c492daf85f922c9dc3b8da341a7d8b95438c1
Andrea Silva <55775461+Andre2797@users.noreply.github.com>
2021-12-18 23:30:07 -0500

    tabla_odontograma

--- Biodentis/frondBiodentis/src/app/app.module.ts
+++ Biodentis/frondBiodentis/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,29 @@ import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from "./auth.guard";
 import { HistoriaClinicaComponent } from './components/historia-clinica/historia-clinica.component';
 import { OdontogramaComponent } from './components/odontograma/odontograma.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { TablaHistoriasClinicasComponent } from './components/tabla-historias-clinicas/tabla-historias-clinicas.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 
 @NgModule({
   declarations: [
@@ -17,14 +40,35 @@ import { OdontogramaComponent } from './components/odontograma/odontograma.compo
     LoginComponent,
     HomeComponent,
     HistoriaClinicaComponent,
-    OdontogramaComponent
-   
+    OdontogramaComponent,
+    TablaHistoriasClinicasComponent,
+    MenuComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatTableModule,
+    FontAwesomeModule,
+   
   ],
   providers: [
     AuthGuard
---
+++ Biodentis/frondBiodentis/src/app/components/tabla-historias-clinicas/tabla-historias-clinicas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-historias-clinicas',
+  templateUrl: './tabla-historias-clinicas.component.html',
+  styleUrls: ['./tabla-historias-clinicas.component.scss']
+})
+export class TablaHistoriasClinicasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
